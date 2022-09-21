b3c048c7037908c8dd8e3856adc5aa594e41154e
GabrielSilvaSouza <gabrieldasilvasouza21@gmail.com>
2022-09-21 17:57:37 -0300

    ll initialized

--- graph.cpp
+++ graph.cpp
@@ -4,52 +4,56 @@
 using namespace std;
 using namespace std::chrono;
 
+class Node
+{
+    public:
+    int data;
+    Node *next;
+};
 
-
-void graphBuilderMatrix(ifstream& infile) {
-
-	int numberVertex;
-	int edge=0;
-	int Vertex_a, Vertex_b;
+bool graphBuilderMatrix(ifstream& infile, int numberVertex, int edge , int Vertex_a, int Vertex_b) {
 	
-	infile >> numberVertex;
-	////----> int *graph = (int*) malloc (sizeof (int) * (n*n)); */
-	bool graph[numberVertex][numberVertex] = {false};
+	bool graph[numberVertex][numberVertex] = {false}; //the memory will explode. must use pointers here
 	
 	while(infile >> Vertex_a >> Vertex_b) {
         graph[Vertex_a][Vertex_b] = true;
         graph[Vertex_b][Vertex_a] = true;
 		edge++;
 	}
-	cout << edge;
-    cout << "\n";
+
+	return graph;
 }
 
-void graphBuilderList(ifstream& infile) {
+
+void graphBuilderLL(int model){
+	int i,j;
+	int n = 0;
+
+	for(i=0; i < n; i++){
+		for(j = 0; j < n; j++) {
+
+			if (i<j) {
+
+			}	
+
+		}
+
+	}
+	
+
+}
+
+int main() {
 
 	int numberVertex;
 	int edge=0;
 	int Vertex_a, Vertex_b;
-	
-	infile >> numberVertex;
-	////----> int *graph = (int*) malloc (sizeof (int) * (n*n)); */
-	bool graph[numberVertex] = {false};
-	
-	/*while(infile >> Vertex_a >> Vertex_b) {
-        graph[Vertex_a][Vertex_b] = true;
-        graph[Vertex_b][Vertex_a] = true;
-		edge++;
-	}*/
-	cout << edge;
-    cout << "\n";
-}
-
-
-
-int main() {
+	bool model;
 	ifstream infile("data.txt");
+	infile >> numberVertex;
 	auto start = high_resolution_clock::now();
-	graphBuilderMatrix(infile);
+	model = graphBuilderMatrix(infile, numberVertex, edge, Vertex_a, Vertex_b);
+	graphBuilderLL(model);
 	auto stop = high_resolution_clock::now();
 	auto duration = duration_cast<microseconds>(stop - start);
     	cout << "Time taken by function: "
