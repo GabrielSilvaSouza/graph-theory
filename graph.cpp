d070e4a77df841d0ab39daa0b4d62c4eff302e16
LucasInocencio <113804674+lucas-inocencio@users.noreply.github.com>
2022-09-26 19:02:44 -0300

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -5,42 +5,70 @@
 using namespace std;
 using namespace std::chrono;
 
-
-class Node
-{
+class Graph {
     public:
-    int data;
-    Node *next;
-};
+        void input() {
 
-bool graphBuilderMatrix(ifstream& infile, int numberVertex, int edge , int Vertex_a, int Vertex_b) {
-	
-	bool graph[numberVertex][numberVertex] = {false}; //the memory will explode. must use pointers here
-	
-	while(infile >> Vertex_a >> Vertex_b) {
-        graph[Vertex_a][Vertex_b] = true;
-        graph[Vertex_b][Vertex_a] = true;
-		edge++;
-	}
-	return graph;
+        }
+
+        void output() {
+
+            cout << Graph.findConnectedComponent()
+
+            return textFile
+        }
+
+        void breadthFirstSearch() {
+
+        }
+
+        void depthFirstSearch() {
+
+        }
+
+        void findDistance() {
+
+        }
+
+        void findDiameter() {
+
+        }
+
+        void findConnectedComponent() {
+
+        }
 }
 
-void graphBuilderLL(vector<bool> graph, int n, std::vector<vector<bool>> model){
-	
-	for(int i=0; i < n; i++){
-		for(int j = 0; j < n; j++) {
-			
-			if (i<j && model[i][j] == true) {
-				//colocar "for" e carregar os objetos
-				graph[i] = {};
-			}
+class AdjacencyMatrix: public Graph {
+    public:
+        bool graphBuilderAdjacencyMatrix(ifstream& infile, int numberVertex, int edge , int Vertex_a, int Vertex_b) {
+        
+        bool graph[numberVertex][numberVertex] = {false};
+        
+        while(infile >> Vertex_a >> Vertex_b) {
+            graph[Vertex_a][Vertex_b] = true;
+            graph[Vertex_b][Vertex_a] = true;
+            edge++;
+        }
 
-		}
-
-	}
-	
-
+        return graph;
+    }
 }
+
+class AdjacencyVector: public Graph {
+    public:
+        void graphBuilderAdjacencyVector(vector<bool> graph, int n, std::vector<vector<bool>> model) {
+        
+        for(int i=0; i < n; i++){
+            for(int j = 0; j < n; j++) {
+                if (i<j && model[i][j] == true) {
+                    //colocar "for" e carregar os objetos
+                    graph[i] = {};
+                }
+            }
+        }
+    }
+} 
 
 int main() {
 
@@ -50,20 +78,16 @@ int main() {
 	infile >> numberVertex;
 	model[numberVertex];
 
-
 	auto start = high_resolution_clock::now();
-
 
 	std::vector<vector<bool>> u { graphBuilderMatrix(infile, numberVertex, edge, Vertex_a, Vertex_b)};
 	//graphBuilderLL(model, numberVertex, u);
 
-
 	auto stop = high_resolution_clock::now();
 	auto duration = duration_cast<microseconds>(stop - start);
     	cout << "Time taken by function: "
-         << duration.count() << " microseconds" << endl;
+        << duration.count() << " microseconds" << endl;
 
 	infile.close();
 	return 0;
-
 }
