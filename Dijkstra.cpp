5d1af68210a0b80b12a42b6297e7c054cb85414d
Skeletor <78266597+GabrielSilvaSouza@users.noreply.github.com>
2022-10-27 18:33:54 -0300

    next step: MST

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -15,64 +15,110 @@ class Graph { //class created
 
     int root; //root will be needed for the heap function
 
-    vector< tuple <int, float> > * v_w;  // a vector of tuple to store number and weight
+    vector< tuple <int, float> > * graphLL;  // a vector of tuple to store number and weight
+
+    int numberVertex;
 
 public:
 
-    void graphBuilderAdjacencyVector(int numberVertex, vector< tuple <int, float> > graphLL[],  int Vertex_a, int Vertex_b); //builder metohd
+    Graph(int n) 
+    {
 
-};
+        ifstream infile("grafo_1.txt"); 
+        int numberVertex;
+        infile >> numberVertex;
+        infile.close();
 
+        this->numberVertex = numberVertex;
 
-void Graph::graphBuilderAdjacencyVector(int numberVertex, vector< tuple <int, float> >  graphLL[], int Vertex_a, int Vertex_b) {
+        graphLL = new vector< tuple <int, float> > [numberVertex+1];
+    }
+
+    void graphBuilderAdjacencyVector(int Vertex_a, int Vertex_b) 
+    {
             
-    int weight;
-    ifstream infile("grafo_1.txt");
-    while(infile >> weight >> Vertex_a >> Vertex_b ) {
+        int weight;
+        int jump;
+        ifstream infile("grafo_1.txt");
+        infile >> weight;
 
-        graphLL[Vertex_a].push_back(make_tuple(Vertex_b,weight)); //insert a linked list inside a vector
-        sort(graphLL[Vertex_a].begin(), graphLL[Vertex_a].end()); //sort the vector 
-        graphLL[Vertex_b].push_back(make_tuple(Vertex_a,weight));
-        sort(graphLL[Vertex_b].begin(), graphLL[Vertex_b].end());
+        while(infile >> Vertex_a >> Vertex_b >> weight ) {
+            graphLL[Vertex_a].push_back(make_tuple(Vertex_b,weight)); //insert a linked list inside a vector
+            sort(graphLL[Vertex_a].begin(), graphLL[Vertex_a].end()); //sort the vector 
+            graphLL[Vertex_b].push_back(make_tuple(Vertex_a,weight));
+            sort(graphLL[Vertex_b].begin(), graphLL[Vertex_b].end());
 
-    // #is it posssible to optimize the sorting function? (it walks on the vector in each loop) --> can it be better?
+        }
+        infile.close();
+    
+    
+
+    } 
+    
+
+    void dijkstra(int start, int end, int numberVertex) 
+    {
+
+        vector<float> dist(numberVertex, numeric_limits<float>::infinity());
+        list<int> explored;
+        explored.push_back(start);
+        list<int> found; 
+        found.push_back(start);
+        dist[start] = 0;
+
+
+        while (explored.size() != dist.size())
+        {
+
+            for (const auto& v : graphLL[start]) 
+            {
+
+                if (std::find(explored.begin(), explored.end(), get<0>(v) ) == explored.end()) {
+                    found.push_back(get<0>(v));
+                }
+                
+                if (dist[get<0>(v)] > (dist[start] + get<1>(v))) {
+                    dist[get<0>(v)] = dist[start] + get<1>(v);
+                }
+
+            }
+
+            start = found.front();
+            explored.push_back(found.front());
+            found.pop_front();
+
+        }
+        
+        for (const auto& i : dist ) {
+            cout << i << endl;
+        }
 
     }
-    infile.close();
-
-/*  //print function to check on vertex
-    for (const auto& i : graphLL[67] ) {
-        cout << get<0>(i) << endl;
-    }
-*/
-}
- 
+  
+};
 
 int main() {
     
-    ifstream infile("grafo_1.txt"); //initialize graph
 
-	int numberVertex, Vertex_a, Vertex_b, edge=0;
-
-    infile >> numberVertex;
-    vector< tuple <int, float> > graphLL[numberVertex+1];
-
-    infile.close(); // end of initialization (obs: the allocation will explode with higher graphs, must use alloc (sizeof())
-    
+    int Vertex_a, Vertex_b;
     clock_t start, end;
     double cpu_time_used;
 
-    Graph u; //create an object
+    Graph u(0); //create an object
 
 	start = clock();
-    
-	u.graphBuilderAdjacencyVector(numberVertex, graphLL,  Vertex_a, Vertex_b); //builder
-
+	u.graphBuilderAdjacencyVector(Vertex_a, Vertex_b); //builder
+    u.dijkstra(2,1,6);
     end = clock();
   
     cpu_time_used = ((double) (end-start)) / CLOCKS_PER_SEC;
-    cout << cpu_time_used;
+    
+    cout << "\n";
+
+    cout << "Tempo gasto: " << cpu_time_used;
    
-	return 0;
+	
+    
+    return 0;
     
 }
