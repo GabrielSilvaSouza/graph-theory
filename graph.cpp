732d638507f0ef057aada31b34f4f521230d7f37
LucasInocencio <113804674+lucas-inocencio@users.noreply.github.com>
2022-11-05 12:31:00 -0300

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -1,93 +1,164 @@
-#include <iostream>
-#include <fstream>
 #include <chrono>
+#include <fstream>
+#include <iostream>
+#include <string>
 #include <vector>
 using namespace std;
-using namespace std::chrono;
+
+class Node_Linked_List {
+
+    public:
+        int data;
+        Node_Linked_List* next_node;
+        float edge_weight;
+
+        Node_Linked_List() {
+            data = 0;
+            next_node = NULL;
+        }
+
+        Node_Linked_List(int data) {
+            this->data = data;
+            this->next_node = NULL;
+        }
+
+        Node_Linked_List(int, float);
+};
+
+Node_Linked_List::Node_Linked_List(int data, float new_edge_weight) {
+    
+    this->data = data;
+    this->next_node = NULL;
+    edge_weight = new_edge_weight;
+}
+
+class Linked_List {
+    
+    public:
+        Node_Linked_List* head_linked_list;
+        int size_linked_list;
+
+        Linked_List() {
+            head_linked_list = NULL;
+        }
+    
+        void insert(int);
+        void insert(int, float);
+        void print_linked_list();
+};
+
+void Linked_List::insert(int data) {
+
+    Node_Linked_List* new_node = new Node_Linked_List(data);
+    new_node->next_node = head_linked_list;
+    this->head_linked_list = new_node;
+    size_linked_list += 1;
+}
+
+void Linked_List::insert(int data, float edge_weight) {
+
+    Node_Linked_List* new_node = new Node_Linked_List(data, edge_weight);
+    new_node->next_node = head_linked_list;
+    this->head_linked_list = new_node;
+    size_linked_list += 1;
+}
+
+void Linked_List::print_linked_list() {
+
+    Node_Linked_List* temporary = head_linked_list;
+
+    if (head_linked_list == NULL) {
+        cout << "List empty" << endl;
+        return;
+    }
+
+    while (temporary != NULL) {
+        cout << temporary->data << " ";
+        temporary = temporary->next_node;
+    }
+}
+
+class Prioriy_Queue {
+
+};
+
+class Binary_Heap {
+
+};
 
 class Graph {
+
     public:
-        void input() {
+        int number_vertex;
+        vector<Linked_List> graph_representation;
 
-        }
+        void graph_builder_adjacency_list(string);
+        float get_distance();
+        void shortest_path();
+        void dijkstra_algorithm(int);
+        void minimum_spanning_tree();
+};
 
-        void output() {
+void Graph::graph_builder_adjacency_list(string file_name) {
 
-            cout << Graph.findConnectedComponent()
+    int number_vertex, vertex_v, neighboring_vertex_v;
+    float edge_weight;
+    ifstream infile(file_name);
+    infile >> number_vertex;
+    graph_representation.resize(number_vertex);
 
-            return textFile
-        }
-
-        void breadthFirstSearch() {
-
-        }
-
-        void depthFirstSearch() {
-
-        }
-
-        void findDistance() {
-
-        }
-
-        void findDiameter() {
-
-        }
-
-        void findConnectedComponent() {
-
-        }
+    while(infile >> vertex_v >> neighboring_vertex_v >> edge_weight) {
+        graph_representation[vertex_v-1].insert(neighboring_vertex_v, edge_weight);
+        graph_representation[neighboring_vertex_v-1].insert(vertex_v, edge_weight);
+    }
+    
+    infile.close();
 }
 
-class AdjacencyMatrix: public Graph {
-    public:
-        bool graphBuilderAdjacencyMatrix(ifstream& infile, int numberVertex, int edge , int Vertex_a, int Vertex_b) {
-        
-        bool graph[numberVertex][numberVertex] = {false};
-        
-        while(infile >> Vertex_a >> Vertex_b) {
-            graph[Vertex_a][Vertex_b] = true;
-            graph[Vertex_b][Vertex_a] = true;
-            edge++;
-        }
+float Graph::get_distance() {
 
-        return graph;
-    }
 }
 
-class AdjacencyVector: public Graph {
-    public:
-        void graphBuilderAdjacencyVector(vector<bool> graph, int n, std::vector<vector<bool>> model) {
-        
-        for(int i=0; i < n; i++){
-            for(int j = 0; j < n; j++) {
-                if (i<j && model[i][j] == true) {
-                    //colocar "for" e carregar os objetos
-                    graph[i] = {};
-                }
-            }
-        }
+void Graph::shortest_path() {
+
+}
+
+void Graph::dijkstra_algorithm(int start_vertex) {
+    
+    vector<float> distance_vector(number_vertex, numeric_limits<float>::infinity());
+    Linked_List explored_set;
+    distance_vector[start_vertex-1] = 0;
+    explored_set.insert(start_vertex);
+
+    for (int i = 0; i < number_vertex; i++) {
     }
-} 
+    
+    for (int i = 0; i < graph_representation[start_vertex].size_linked_list; i++) {
+        Node_Linked_List* temporary = graph_representation[start_vertex].head_linked_list;
+        distance_vector[temporary->data] = temporary->edge_weight;
+        temporary = temporary->next_node;
+    }
+
+    while(explored_set.size_linked_list != number_vertex) {
+        
+    }
+
+    for (const auto& i : distance_vector) {
+        cout << i << endl;
+    }
+
+    //return distance_vector
+}
+
+void Graph::minimum_spanning_tree() {
+
+}
 
 int main() {
 
-	int numberVertex,Vertex_a, Vertex_b, edge=0;
-	vector<bool> model;
-	ifstream infile("data.txt");
-	infile >> numberVertex;
-	model[numberVertex];
+    Graph graph_test;
 
-	auto start = high_resolution_clock::now();
-
-	std::vector<vector<bool>> u { graphBuilderMatrix(infile, numberVertex, edge, Vertex_a, Vertex_b)};
-	//graphBuilderLL(model, numberVertex, u);
-
-	auto stop = high_resolution_clock::now();
-	auto duration = duration_cast<microseconds>(stop - start);
-    	cout << "Time taken by function: "
-        << duration.count() << " microseconds" << endl;
-
-	infile.close();
-	return 0;
+    graph_test.graph_builder_adjacency_list("grafo_W_5_1.txt");
+    
+    return 0;
 }
